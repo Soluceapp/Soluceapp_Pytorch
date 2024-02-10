e54502b1fe5098bfabd580f1ca41d55c41a6c0fa
Soluceapp <thomas.vervisch@soluceapp.com>
2024-02-10 14:31:08 +0100

    Rect  structure

--- src/connexion/static/js/script_pytorch.js
+++
@@ -1,5 +0,0 @@
-function sub_menu35() {window.location.assign("http://localhost:20000/board/");}
-function sub_menu37() {window.location.assign("http://localhost:20000/board/configuration_data");}
-function sub_menu38() {window.location.assign("http://localhost:20000/board/configuration_IA");}
-function sub_menu39() {window.location.assign("http://localhost:20000/board/previsions");}
-function sub_menu5() {document.getElementById("myDropdown5").classList.toggle("show");}
