e7fc4c07acb0091ce1f048af71b36a4db8895357
Soluceapp <thomas.vervisch@soluceapp.com>
2024-02-10 01:33:12 +0100

    Rect  static js et css

---
+++ src/connexion/static/js/script_pytorch.js
@@ -0,0 +1,7 @@
+function sub_menu35() {window.location.assign("http://localhost:8000/activities");}
+function sub_menu37() {window.location.assign("http://localhost:8000/profile_student");}
+function sub_menu38() {window.location.assign("http://localhost:8000/profile_student/changepseudo");}
+function sub_menu39() {window.location.assign("http://localhost:8000/profile_student/anciencours");}
+function sub_menu40() {window.location.assign("http://localhost:8000/profile_student/changeclasse");}
+function sub_menu42() {window.location.assign("https://palissy.soluceapp.com");}
+function sub_menu5() {document.getElementById("myDropdown5").classList.toggle("show");}
